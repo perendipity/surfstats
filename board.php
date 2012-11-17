3a29f74447edfeed023782a96e7fe4088333918d
marco.morawec <marco.morawec@gmail.com>
2012-11-17 16:00:35 -0500

    put js into the footer

--- board.php
+++ board.php
@@ -81,24 +81,4 @@ include('./includes/sidebar.inc.php');
 
 <?php include('./includes/footer.inc.php'); ?>
 
-<!-- End of the footer -->
 
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap-transition.js"></script>
-    <script src="js/bootstrap-alert.js"></script>
-    <script src="js/bootstrap-modal.js"></script>
-    <script src="js/bootstrap-dropdown.js"></script>
-    <script src="js/bootstrap-scrollspy.js"></script>
-    <script src="js/bootstrap-tab.js"></script>
-    <script src="js/bootstrap-tooltip.js"></script>
-    <script src="js/bootstrap-popover.js"></script>
-    <script src="js/bootstrap-button.js"></script>
-    <script src="js/bootstrap-collapse.js"></script>
-    <script src="js/bootstrap-carousel.js"></script>
-    <script src="js/bootstrap-typeahead.js"></script>
-
-  </body>
-</html>
