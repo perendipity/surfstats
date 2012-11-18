d1b2361294511e5821fd5de37396ecaff9e64659
marco.morawec <marco.morawec@gmail.com>
2012-11-18 05:50:17 -0500

    take out of responsive design

--- home.php
+++ home.php
@@ -67,10 +67,7 @@
    </div> <!-- well-clear -->
 
     <!-- FOOTER -->
-    <footer>
-      <p class="pull-right"><a href="#">Back to top</a></p>
-      <p>&copy; 2012 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-    </footer>
+<?php include('./includes/footer.inc.php'); ?>
 
   </div><!-- /.container -->
 
