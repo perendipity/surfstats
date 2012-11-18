165dce9757e98f35ad873049c1924f66360c219c
Cody Canning <cocanning11@gmail.com>
2012-11-17 19:40:02 -0500

    added some table stying to surfscore.css

--- leaderboard.php
+++ leaderboard.php
@@ -8,176 +8,198 @@ include('./includes/sidebar.inc.php');
 <!-- Header to the Leaderboards page -->
     <h1 align="center">Leaderboards</h1>
 
-    <!-- Accordion contains different leaderboards -->
-    <div class="accordion" id="leaderboard-charts">
-      <div class="accordion-group">
-        <div class="accordion-heading">
-          <a class="accordion-toggle" data-toggle="collapse" data-parent="#leaderboard-charts" href="#friends">
-            Friends
-          </a>
+    
+    <div class="container">
+      <!-- Accordion contains different leaderboards -->
+      <div class="accordion" id="leaderboard-charts">
+        <div class="accordion-group">
+          <div class="accordion-heading">
+            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leaderboard-charts" href="#friends">
+              Friends
+            </a>
+          </div>
+          <div id="friends" class="accordion-body collapse in">
+            <div class="accordion-inner">              
+              <table class="table tablesorter table-striped">
+                <thead>
+                  <tr>
+                    <th>Name 
+                      <i class="icon-chevron-down pull-right"> </i> 
+                      <i class="icon-chevron-up pull-right"> </i> 
+                    </th>
+                    <th>Best Sesh
+                      <i class="icon-chevron-down pull-right"> </i> 
+                      <i class="icon-chevron-up pull-right"> </i> 
+                    </th>
+                    <th>Average Sesh
+                      <i class="icon-chevron-down pull-right"> </i> 
+                      <i class="icon-chevron-up pull-right"> </i> 
+                    </th>
+                    <th>Waves
+                      <i class="icon-chevron-down pull-right"> </i> 
+                      <i class="icon-chevron-up pull-right"> </i> 
+                    </th>
+                    <th>Tricks
+                      <i class="icon-chevron-down pull-right"> </i> 
+                      <i class="icon-chevron-up pull-right"> </i>
+                    </th>
+                    <th>Set Waves
+                      <i class="icon-chevron-down pull-right"> </i> 
+                      <i class="icon-chevron-up pull-right"> </i> 
+                    </th>
+                    <th>Water Time   
+                      <i class="icon-chevron-down pull-right"> </i> 
+                      <i class="icon-chevron-up pull-right"> </i> 
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>Bethany Warburton</th>
+                    <td>9.9</td>
+                    <td>8.7</td>
+                    <td>6783</td>
+                    <td>12345</td>
+                    <td>456</td>
+                    <td>763</td>
+                  </tr>
+                  <tr>
+                    <td>Sven Alsvik</td>
+                    <td>9.8</td>
+                    <td>8.6</td>
+                    <td>5463</td>
+                    <td>10020</td>
+                    <td>400</td>
+                    <td>654</td>
+                  </tr>
+                  <tr>
+                    <td>Natalia Olsen</td>
+                    <td>5.6</td>
+                    <td>3.4</td>
+                    <td>200</td>
+                    <td>554</td>
+                    <td>23</td>
+                    <td>107</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-        <div id="friends" class="accordion-body collapse in">
-          <div class="accordion-inner">              
-            <table class="table tablesorter table-striped">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Best Sesh</th>
-                  <th>Average Sesh</th>
-                  <th>Waves</th>
-                  <th>Tricks</th>
-                  <th>Set Waves</th>
-                  <th>Water Time</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th>Bethany Warburton</th>
-                  <td>9.9</td>
-                  <td>8.7</td>
-                  <td>6783</td>
-                  <td>12345</td>
-                  <td>456</td>
-                  <td>763</td>
-                </tr>
-                <tr>
-                  <td>Sven Alsvik</th>
+        <div class="accordion-group">
+          <div class="accordion-heading">
+            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leaderboard-charts" href="#national">
+              National
+            </a>
+          </div>
+          <div id="national" class="accordion-body collapse">
+            <div class="accordion-inner">
+              <table class="table tablesorter table-striped">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Best Sesh</th>
+                    <th>Average Sesh</th>
+                    <th>Waves</th>
+                    <th>Tricks</th>
+                    <th>Set Waves</th>
+                    <th>Water Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>Bethany Warburton</th>
+                    <td>9.9</td>
+                    <td>8.7</td>
+                    <td>6783</td>
+                    <td>12345</td>
+                    <td>456</td>
+                    <td>763</td>
+                  </tr>
+                  <tr>
+                    <td>Sven Alsvik</td>
+                    <td>9.8</td>
+                    <td>8.6</td>
+                    <td>5463</td>
+                    <td>10020</td>
+                    <td>400</td>
+                    <td>654</td>
+                  </tr>
+                  <tr>
+                    <td>Natalia Olsen</td>
+                    <td>5.6</td>
+                    <td>3.4</td>
+                    <td>200</td>
+                    <td>554</td>
+                    <td>23</td>
+                    <td>107</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-group">
+          <div class="accordion-heading">
+            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leaderboard-charts" href="#international">
+              International
+            </a>
+          </div>
+          <div id="international" class="accordion-body collapse">
+            <div class="accordion-inner">
+              <table class="table tablesorter table-striped">
+                <thead>
+                  <tr>
+                    <th>Name </th>
+                    <th>Best Sesh</th>
+                    <th>Average Sesh</th>
+                    <th>Waves</th>
+                    <th>Tricks</th>
+                    <th>Set Waves</th>
+                    <th>Water Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>Bethany Warburton</th>
+                    <td>9.9</td>
+                    <td>8.7</td>
+                    <td>6783</td>
+                    <td>12345</td>
+                    <td>456</td>
+                    <td>763</td>
+                  </tr>
+                  <tr>
+                  <td>Sven Alsvik</td>
                   <td>9.8</td>
                   <td>8.6</td>
                   <td>5463</td>
                   <td>10020</td>
                   <td>400</td>
                   <td>654</td>
-                </tr>
-                <tr>
-                  <td>Natalia Olsen</td>
-                  <td>5.6</td>
-                  <td>3.4</td>
-                  <td>200</td>
-                  <td>554</td>
-                  <td>23</td>
-                  <td>107</td>
-                </tr>
-              </tbody>
-            </table>
+                  </tr>
+                  <tr>
+                    <td>Natalia Olsen</td>
+                    <td>5.6</td>
+                    <td>3.4</td>
+                    <td>200</td>
+                    <td>554</td>
+                    <td>23</td>
+                    <td>107</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-      <div class="accordion-group">
-        <div class="accordion-heading">
-          <a class="accordion-toggle" data-toggle="collapse" data-parent="#leaderboard-charts" href="#national">
-            National
-          </a>
-        </div>
-        <div id="national" class="accordion-body collapse">
-          <div class="accordion-inner">
-            <table class="table tablesorter table-striped">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Best Sesh</th>
-                  <th>Average Sesh</th>
-                  <th>Waves</th>
-                  <th>Tricks</th>
-                  <th>Set Waves</th>
-                  <th>Water Time</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th>Bethany Warburton</th>
-                  <td>9.9</td>
-                  <td>8.7</td>
-                  <td>6783</td>
-                  <td>12345</td>
-                  <td>456</td>
-                  <td>763</td>
-                </tr>
-                <tr>
-                  <td>Sven Alsvik</th>
-                  <td>9.8</td>
-                  <td>8.6</td>
-                  <td>5463</td>
-                  <td>10020</td>
-                  <td>400</td>
-                  <td>654</td>
-                </tr>
-                <tr>
-                  <td>Natalia Olsen</td>
-                  <td>5.6</td>
-                  <td>3.4</td>
-                  <td>200</td>
-                  <td>554</td>
-                  <td>23</td>
-                  <td>107</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-      <div class="accordion-group">
-        <div class="accordion-heading">
-          <a class="accordion-toggle" data-toggle="collapse" data-parent="#leaderboard-charts" href="#international">
-            International
-          </a>
-        </div>
-        <div id="international" class="accordion-body collapse">
-          <div class="accordion-inner">
-            <table class="table tablesorter table-striped">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Best Sesh</th>
-                  <th>Average Sesh</th>
-                  <th>Waves</th>
-                  <th>Tricks</th>
-                  <th>Set Waves</th>
-                  <th>Water Time</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th>Bethany Warburton</th>
-                  <td>9.9</td>
-                  <td>8.7</td>
-                  <td>6783</td>
-                  <td>12345</td>
-                  <td>456</td>
-                  <td>763</td>
-                </tr>
-                <tr>
-                  <td>Sven Alsvik</th>
-                  <td>9.8</td>
-                  <td>8.6</td>
-                  <td>5463</td>
-                  <td>10020</td>
-                  <td>400</td>
-                  <td>654</td>
-                </tr>
-                <tr>
-                  <td>Natalia Olsen</td>
-                  <td>5.6</td>
-                  <td>3.4</td>
-                  <td>200</td>
-                  <td>554</td>
-                  <td>23</td>
-                  <td>107</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+    </div>
   </body>
   <?php include('./includes/footer.inc.php'); ?>
   <script type="text/javascript" id="js">
     $(document).ready(function() { 
       // call the tablesorter plugin 
-      $("table").tablesorter({ 
-        // sort on the first column, order desc. 
-        sortList: [[1,1]] 
-      }); 
+      $("table").tablesorter({sortList: [[1,1]],}); 
     }); 
   </script>
 </html>
