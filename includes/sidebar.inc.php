0b3bd59fb8bc3288914dbba663bbbbec00b79634
Cody Canning <cocanning11@gmail.com>
2012-11-18 16:56:56 -0500

    fixed crown icon navigation to leaderboard

--- includes/sidebar.inc.php
+++ includes/sidebar.inc.php
@@ -1,9 +1,9 @@
 <div class="btn-group btn-group-vertical">
   <ul class="nav nav-list bs-docs-sidenav affix" id="sidebar">
-  <li class="#profile-container"><a href="#profile-container"><div class="sidebar-img"><img border="0" src="img/profilesidenav.png" width="30" height="30"/></div></a></li>
-  <li class="#lifetime-container"><a href="#lifetime-container"><div class="sidebar-img"><img border="0" src="img/lifetimesidenav.png" width="30" height="30"/></div></a></li>
-  <li class="#drilldown-container"><a href="#drilldown-container"><div class="sidebar-img"><img border="0" src="img/drilldownsidenav.png" width="30" height="30"/></div></a></li>
-  <li class="#test"><a href="#test"><div class="sidebar-img"><img border="0" src="img/momentssidenav.png" width="30" height="30"/></div></a></li>
+    <li class="#profile-container"><a href="#profile-container"><div class="sidebar-img"><img border="0" src="img/profilesidenav.png" width="30" height="30"/></div></a></li>
+    <li class="#lifetime-container"><a href="#lifetime-container"><div class="sidebar-img"><img border="0" src="img/lifetimesidenav.png" width="30" height="30"/></div></a></li>
+    <li class="#drilldown-container"><a href="#drilldown-container"><div class="sidebar-img"><img border="0" src="img/drilldownsidenav.png" width="30" height="30"/></div></a></li>
+    <li class="#test"><a href="leaderboard.php"><div class="sidebar-img"><img border="0" src="img/momentssidenav.png" width="30" height="30"/></div></a></li>
   </ul>
 </div>
 
