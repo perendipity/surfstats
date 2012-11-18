ae2a20178faa68b612c000a4407f93ade1c3d158
Xerox <xerox@home>
2012-11-18 06:41:49 -0500

    compressed image as background

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -14,7 +14,7 @@
             <span class="icon-bar"></span>
           </a>
           <img class="logo pull-left" style="padding: 2px 4px 2px 2px;" href="home.php" src="./img/logonav_small.png" class="pull-left">
-          <a class="brand" href="./home.php">Surfscore</a>
+          <a class="brand" href="./home.php">surfscore</a>
           <div class="nav-collapse collapse">
             <ul class="nav">
             <ul class="nav">
@@ -24,7 +24,7 @@
                  echo 'class="active"'; } ?>><a href="board.php">dashboard</a></li>
               <li <?php if ($currentPage == 'leaderboard.php') {
                  echo 'class="active"'; } ?>><a href="leaderboard.php">leaderboard</a></li>
-              <li><a><div id="user-info" style="margin-left: 387px">Marco Morawec <i class="icon-leaf"></i></div></a></li>
+              <li><a><div id="user-info" style="margin-left: 387px">Marco Morawec <img border="0" style="height: 20px; width: 20px" src="./img/avatardot.png"></div></a></li>
           </div><!--/.nav-collapse -->
         </div><!-- /.navbar-inner -->
       </div><!-- /.navbar -->
