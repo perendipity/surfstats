4e633dccad9b2e6a9e45cc9c8a85f548f1a09b75
peternachbaur <pnachbaur@turbine>
2012-11-19 16:31:50 -0500

    added images and content to team page

--- team.php
+++ team.php
@@ -2,7 +2,22 @@
     <?php include('./includes/navbar.inc.php'); ?>
     <body>
         <div class="container board-transparent">
-            Marco Morawec, Peter Nachbaur, Alec Babala, Cody Canning, Nishan Subedi
+            <h1 class="h1-custom">Our Team</h1>
+            <h2 class = "h2-custom">Marco Morawec</h2>
+            <img src="./img/team/marco.jpg">
+            <h3>Product and UX. Surfer</h3>
+            <h2 class = "h2-custom">Peter Nachbaur</h2>
+            <img src="./img/team/peter.jpg">
+            <h3>Big data technologist</h3>
+            <h2 class = "h2-custom">Alec Babala</h2>
+            <img src="./img/team/alec.jpg">
+            <h3>Primo designer</h3>
+            <h2 class = "h2-custom">Cody Canning</h2>
+            <img src="./img/team/cody.jpg">
+            <h3>Machine Learning</h3>
+            <h2 class = "h2-custom">Nishan Subedi</h2>
+            <img src="./img/team/nishan.jpg">
+            <h3>Developer</h3>
         </div> <!-- /container -->
     </body>
 
