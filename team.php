7cf0d0877c8b43720f78e533fdc7fa990d797711
peternachbaur <pnachbaur@turbine>
2012-11-19 16:19:40 -0500

    added team page, should flesh out

---
+++ team.php
@@ -0,0 +1,11 @@
+    <?php include('./includes/header.inc.php'); ?>
+    <?php include('./includes/navbar.inc.php'); ?>
+    <body>
+        <div class="container board-transparent">
+            Marco Morawec, Peter Nachbaur, Alec Babala, Cody Canning, Nishan Subedi
+        </div> <!-- /container -->
+    </body>
+
+<!-- Start of the footer -->
+    <?php include('./includes/footer.inc.php'); ?>
+</html>
