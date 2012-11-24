6189628bda5425120843b942f8e596a0f840c87f
marco.morawec <marco.morawec@gmail.com>
2012-11-23 21:02:16 -0500

    adjusted launchrock widget in registration.php

--- registration.php
+++ registration.php
@@ -3,7 +3,7 @@
     <body>
         <div class="row">
             <div class="span6 offset7" style="margin-top: 100px;">
-                <div rel="YZJSE7VS" class="lrdiscoverwidget" data-logo="on" data-background="off" data-share-url="surfscore.me/signup" data-css="">
+                <div rel="YZJSE7VS" class="lrdiscoverwidget" data-logo="on" data-background="off" data-share-url="surfscore.me/registration.php " data-css="">
             </div>
             <script type="text/javascript" src="http://launchrock-ignition.s3.amazonaws.com/ignition.1.1.js"></script>
             </div>
