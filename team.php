9cab55956009f86357ba114d572d82350eee1824
marco.morawec <marco.morawec@gmail.com>
2012-11-27 18:33:25 -0500

    team.php exchange

--- team.php
+++ team.php
@@ -2,24 +2,19 @@
     <?php include('./includes/navbar.inc.php'); ?>
     <body>
         <div class="container board-transparent">
-            <h1 class=" span6 offset1 custom-h1" style="padding:50px 0 30px 0;">Our Team</h1>
-            
-            <!-- Marcos Info -->
-            <div class="row">
-                <div class="span10 offset1">
-                    <!-- <h3>Product and UX</h3> -->
-                    <img style="margin-left: 20px;" src="./img/team/marco_bw.jpg">
+            <h1 class="custom-h1" style="margin:50px">Our Team</h1>
+            <div class="teambox row-fluid" style="margin-left:50px"> <!-- class="teambox" wasn't working...-->
+                <div class="teammember span3" style="margin-bottom: 100px"> 
                     <h2 class="custom-h2">
                         Marco Morawec
                         <a href="http://www.linkedin.com/in/marcomorawec" target="_blank">
                             <img style="margin-left:5px" src="./img/social/linkedin_icon.png">
                         </a>
                     </h2>
+                    <!-- <h3>Product and UX</h3> -->
+                    <img style="margin-left: 45px;" src="./img/team/marco.jpg">
                 </div>
-            <div>
-                
-            <!-- Peters Info -->
-                <div class="span3" style="margin-bottom: 100px">
+                <div class="teammember span3" style="margin-bottom: 100px">
                     <h2 class="custom-h2">
                         Peter Nachbaur
                         <a href="http://www.linkedin.com/pub/peter-nachbaur/44/505/252" target="_blank">
@@ -27,11 +22,9 @@
                         </a>
                     </h2>
                     <!-- <h3>Big data technologist</h3> -->
-                    <img style="margin-left: 45px;" src="./img/team/peter_bw.jpg">
+                    <img style="margin-left: 45px;" src="./img/team/peter.jpg">
                 </div>
-                
-            <!-- Alecs Info -->
-                <div class="span3" style="margin-bottom: 100px;">
+                <div class="teammember span3" style="margin-bottom: 100px;">
                     <h2 class="custom-h2">
                         Alec Babala
                         <a href="http://www.linkedin.com/pub/alec-babala/42/b10/150" target="_blank">
@@ -41,9 +34,7 @@
                     <!-- <h3>Primo designer</h3> -->
                     <img style="margin-left: 15px;" src="./img/team/alec.jpg">
                 </div>
-                
-            <!-- Codys Info -->
-                <div class="span3" style="margin-bottom: 100px; margin-left:-35px">                
+                <div class="teammember span3" style="margin-bottom: 100px; margin-left:-35px">                
                     <h2 class="custom-h2">
                         Cody Canning
                         <a href="http://www.linkedin.com/in/codycanning" target="_blank">
@@ -51,7 +42,7 @@
                         </a>
                     </h2>
                     <!-- <h3>Machine Learning</h3> -->
-                    <img style="margin-left: 45px;" src="./img/team/cody_bw.jpg">
+                    <img style="margin-left: 45px;" src="./img/team/cody.jpg">
                 </div>
 
             </div>
