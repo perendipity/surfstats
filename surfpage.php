7914e527456152b31e3d0a670892ac672d1d3cfe
marco.morawec <marco.morawec@gmail.com>
2012-12-08 19:52:43 -0500

    first changes to the template files

--- surfpage.php
+++ surfpage.php
@@ -21,10 +21,79 @@
 
 </head>
 
-	<?php startblock('title') ?> 
+<!-- Start of Sign-up page -->
+
+	<?php startblock('signup_title') ?> 
 		Don't fall behind. Get surfscore and Surf better!
 	<?php endblock() ?> 
 	
-	<?php startblock('article') ?> 
+	<?php startblock('signup_article') ?> 
 		This is not another silly surf camp. This is a personalized tracking system, allowing you to see what's going on the water.  
 	<?php endblock() ?>
+	
+<!-- End of Sign-up page -->
+
+
+<!-- Start of How it works page -->
+
+<?php startblock('howitworks_title') ?> 
+       <div class="span4">
+		<img class="img-circle" src="http://placehold.it/140x140">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <img class="img-circle" src="http://placehold.it/140x140">
+          <h2>Heading</h2>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <img class="img-circle" src="http://placehold.it/140x140">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div><!-- /.span4 -->
+      
+<?php endblock() ?> 
+	
+<!-- End of How it works page -->
+
+
+<!-- Start of Data page -->
+
+	<?php startblock('data_title') ?> 
+		This is data we will show you!
+	<?php endblock() ?> 
+<!-- End of Data page -->
+
+
+<!-- Start of Surfboard page -->
+	<?php startblock('surfboard_title') ?> 
+		This is how we put stuff on your surfboard!
+	<?php endblock() ?> 
+<!-- End of surfboard page -->
+
+
+<!-- Start of Leaderboard page -->
+	<?php startblock('leaderboard_title') ?> 
+		Aim for world domination!
+	<?php endblock() ?> 
+<!-- End of Leaderboard page -->
+
+
+<!-- Start of Kick it page -->
+	<?php startblock('kickit_title') ?> 
+		This is how it works!
+	<?php endblock() ?> 
+<!-- End of Kick it page -->
+
+
+<!-- Start of Team page -->
+	<?php startblock('team_title') ?> 
+		This is how it works!
+	<?php endblock() ?> 
+<!-- End of Team page -->
+
+
--- template.php
+++ template.php
@@ -33,43 +33,74 @@
 	<div class="span8">
 	
 		<h1>
-			<?php startblock('title') ?> 
+			<?php startblock('signup_title') ?> 
 			<?php endblock() ?> 
 		</h1> 
 		<div id='article'> 
-			<?php startblock('article') ?> 
+			<?php startblock('signup_article') ?> 
 			<?php endblock() ?> 
 		</div> 
 	</div>
 	
 </div> <!-- End container -->
 
-    <!-- HOW IT WORKS. First -> Second -> Third
-    ================================================== -->
+<!-- HOW IT WORKS. First -> Second -> Third
+================================================== -->
 
     <div class="container marketing">
 
       <!-- Three columns of text below the carousel -->
       <div class="row">
-        <div class="span4">
-          <img class="img-circle" src="http://placehold.it/140x140">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div><!-- /.span4 -->
-        <div class="span4">
-          <img class="img-circle" src="http://placehold.it/140x140">
-          <h2>Heading</h2>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div><!-- /.span4 -->
-        <div class="span4">
-          <img class="img-circle" src="http://placehold.it/140x140">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div><!-- /.span4 -->
+			<?php startblock('howitworks_title') ?> 
+			<?php endblock() ?> 
       </div><!-- /.row -->
+<!-- DATA PAGE
+================================================== -->
+
+		<h1>
+			<?php startblock('data_title') ?> 
+			<?php endblock() ?> 
+		</h1> 
+
+<!-- SURFBOARD PAGE
+================================================== -->
+
+		<h1>
+			<?php startblock('surfboard_title') ?> 
+			<?php endblock() ?> 
+		</h1> 
+
+
+<!-- LEADERBOARD PAGE
+================================================== -->
+
+		<h1>
+			<?php startblock('leaderboard_title') ?> 
+			<?php endblock() ?> 
+		</h1> 
+
+
+<!-- KICK IT PAGE
+================================================== -->
+
+		<h1>
+			<?php startblock('kickit_title') ?> 
+			<?php endblock() ?> 
+		</h1> 
+
+
+<!-- TEAM PAGE
+================================================== -->
+
+		<h1>
+			<?php startblock('team_title') ?> 
+			<?php endblock() ?> 
+		</h1> 
+
+
+
+
+
 
 
 
