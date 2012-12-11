d25603cb8b5a198ab887c3d722a19c720a968602
marco.morawec <marco.morawec@gmail.com>
2012-12-10 21:40:13 -0500

    changes to the new signup form

--- registration_new.php
+++ registration_new.php
@@ -10,10 +10,20 @@
 	            	<h5 class="custom-h4">We update you via email when we're ready</h5></br>
 					<!-- Begin email form -->
 					<form id="email" action="./includes/db_surfscore.inc.php" method="post">
-					   		<div class="email input-append">
+					   		<div id="signup-box signup-container" class="email input-append">
 					   			<input size="20" id="appendedInputButton" type="email" required="" placeholder="Your email address" name="email">
-					    		<button class="btn btn-danger" type="submit">»</button>
-					    	</div>				
+					    		<button id="signup-button" class="btn btn-danger" type="submit">»</button>
+					    	</div>	
+						<script>					    	
+							$(document).ready(function() {
+							
+							    $('.signup-button').submit(function() {
+							      $('.signup-box').hide();
+							      $('.signup-container').append('<p class ="signup-thanks">Thanks!</p>')
+							    });
+							
+							 });	
+						 </script>				    				
 					</form>
 				</div> <!-- End of signup-box -->
 					
@@ -21,3 +31,4 @@
         </div>
     </body>
 </html>
+
