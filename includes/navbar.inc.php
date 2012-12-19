2e3f6d086f4b8ac488f2822f38938106027cc8d8
marco.morawec <marco.morawec@gmail.com>
2012-12-18 19:12:38 -1000

    changes to navbar links

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -2,17 +2,17 @@
     ================================================== -->
     <div class="container navbar-wrapper">
       <div class="navbar navbar-inverse">
-          <a class="surfscore-brand" href="surfpage.php">surfscore</a>
+          <a class="surfscore-brand" href="home.php">surfscore</a>
           <div class="nav-collapse">
             <ul class="nav pull-right">
               <li <?php if ($currentPage == 'home.php') {
-                 echo 'class="active"'; } ?>><a href="surfpage.php">home</a></li>
+                 echo 'class="active"'; } ?>><a href="home.php">home</a></li>
               <li <?php if ($currentPage == 'team.php') {
-                echo 'class="active"'; } ?>><a href="#team-section">team</a></li>
-              <li <?php if ($currentPage == 'dontbeakook.tumblr.com') {
-                echo 'class="active"'; } ?>><a href="#news-section">blog</a></li>
+                echo 'class="active"'; } ?>><a href="team.php">team</a></li>
+              <li <?php if ($currentPage == 'news.php') {
+                echo 'class="active"'; } ?>><a href="news.php">news</a></li>
               <li <?php if ($currentPage == 'contact.php') {
-                echo 'class="active"'; } ?>><a href="contact.php">contact</a></li>
+                echo 'class="active"'; } ?>><a href="#">contact</a></li>
             </ul>            
 		</div>
 	</div>
