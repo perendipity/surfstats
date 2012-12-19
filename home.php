e51d99096fad4e7448dafa49a677f82830e25a48
marco.morawec <marco.morawec@gmail.com>
2012-12-18 18:51:56 -1000

    adding pictures and sidebar

---
+++ home.php
@@ -0,0 +1,175 @@
+
+
+
+<?php include './includes/template.home.php' ?> 
+
+<!-- Surf page specific styles
+================================================== -->
+
+<style>
+	body {
+			background-color: black;
+			color: #FFFFFF;
+		}
+	
+</style>
+
+
+
+</head>
+
+<!-- Start of Sign-up page -->
+
+	<?php startblock('signup_title') ?> 
+		Improve Your Surfing
+	<?php endblock() ?> 
+
+	<?php startblock('signup_h2') ?> 
+		Track Each Wave. Surf Better</br> 
+	<?php endblock() ?>
+	
+	<?php startblock('signup_copy') ?> 
+		This is the best thing since sliced bread. Track all your metrics, get better, see what's happening
+		and improve consistently. Compete with friends and be the best surfer you could possibly be.  
+	<?php endblock() ?>
+
+	<?php startblock('signup_emailform') ?> 
+		<!-- Begin email form -->
+			<form id="email" action="" method="post">
+			   	<div class="email input-append">
+			   		<input size="20" id="appendedInputButton" type="email" required="" placeholder="Your email address" name="email">
+			    	<button class="btn btn-danger" type="submit">»</button>
+			    </div>				
+			</form>
+	<?php endblock() ?>
+
+	<?php startblock('signup_image') ?> 
+		<img src="./img/surfscore/kooks.png" >  
+	<?php endblock() ?>
+
+	<?php startblock('signup_sidebar') ?> 
+		<?php include './includes/sidebar.inc.php'; ?>
+	<?php endblock() ?>
+
+<!-- End of Sign-up page -->
+
+
+<!-- Start of How it works page -->
+
+		<?php startblock('howitworks_title') ?> 
+			How it works
+		<?php endblock() ?> 
+
+		<?php startblock('howitworks_point1') ?> 
+			<img class="" src="./img/surfscore/shred-o-meter.png">
+			</br>
+	        	<h2 class="custom-h2">Mount Your surfscore</h2>
+	        	<h5 class="custom-h5">One small device to track all your data</h5>
+	        	<p><a class="btn btn-success" href="#data-section">View details &raquo;</a></p>
+		<?php endblock() ?> 
+
+		<?php startblock('howitworks_point2') ?> 
+			<img class="img-circle" src="./img/surfscore/surfing.png">
+			</br>
+	          	<h2 class="custom-h2">Go Surfing</h2>
+	          	<h5 class="custom-h5">Surf hard. Go Radical. Be Yourself!</h5>
+	          	<p><a class="btn btn-success" href="#">View details &raquo;</a></p>
+		<?php endblock() ?> 
+		
+		<?php startblock('howitworks_point3') ?> 
+			<img class="img-circle" src="./img/surfscore/surf_data.png">
+			</br>
+		        <h2 class="custom-h2">Get Detailed Feedback</h2>
+		        <h5 class="custom-h5">Waves. Shredder Score. Ride Visualization</h5>
+		        <p><a class="btn btn-success" href="#">View details &raquo;</a></p>
+		<?php endblock() ?> 
+      
+	
+<!-- End of How it works page -->
+
+
+<!-- Start of Data page -->
+
+	<?php startblock('data_title') ?> 
+		See Every Wave
+	<?php endblock() ?> 
+	
+	<?php startblock('data_h2') ?> 
+		Get Your Stats &</br> 
+		Know What's Happening!  
+	<?php endblock() ?>
+	
+	<?php startblock('data_copy') ?> 
+		This is the best thing since sliced bread. Track all your metrics, get better, see what's happening
+		and improve consistently. Compete with friends and be the best surfer you could possibly be.  
+	<?php endblock() ?>
+
+	<?php startblock('data_image') ?> 
+		<img class="img-home" src="./img/surfscore/surf_data2.png" >  
+	<?php endblock() ?>
+
+	<?php startblock('data_sidebar') ?> 
+		<?php include './includes/sidebar.inc.php'; ?>
+	<?php endblock() ?>	
+	
+<!-- End of Data page -->
+
+
+<!-- Start of Surfboard page -->
+
+	<?php startblock('surfboard_title') ?> 
+		One Simple Device
+	<?php endblock() ?> 
+	
+	<?php startblock('surfboard_h2') ?> 
+		Smaller than a fin-plug.</br> 
+		Battery Power for 6 Month!
+	<?php endblock() ?>
+	
+	<?php startblock('surfboard_copy') ?> 
+		This is the best thing since sliced bread. Track all your metrics, get better, see what's happening
+		and improve consistently. Compete with friends and be the best surfer you could possibly be.  
+	<?php endblock() ?>
+
+	<?php startblock('surfboard_image') ?> 
+		<img class="img-home" src="./img/surfscore/shred-o-meter.png" >  
+	<?php endblock() ?>
+
+	<?php startblock('surfboard_sidebar') ?> 
+		<?php include './includes/sidebar.inc.php'; ?>
+	<?php endblock() ?>	
+	
+<!-- End of surfboard page -->
+
+
+<!-- Start of Leaderboard page -->
+
+	<?php startblock('leaderboard_title') ?> 
+		Surf Hard. Compete!
+	<?php endblock() ?> 
+	
+	<?php startblock('leaderboard_h2') ?> 
+		Challenge Your Friends</br> 
+		Dominate the Leaderboard!  
+	<?php endblock() ?>
+	
+	<?php startblock('leaderboard_copy') ?> 
+		This is the best thing since sliced bread. Track all your metrics, get better, see what's happening
+		and improve consistently. Compete with friends and be the best surfer you could possibly be.  
+	<?php endblock() ?>
+
+	<?php startblock('leaderboard_image') ?> 
+		<img class="img-home" src="./img/surfscore/iphone_template2.png" >  
+	<?php endblock() ?>
+
+	<?php startblock('leaderboard_sidebar') ?> 
+		<?php include './includes/sidebar.inc.php'; ?>
+	<?php endblock() ?>	
+
+<!-- End of Leaderboard page -->
+
+
+
+
+
+
---
+++ includes/sidebar.inc.php
@@ -0,0 +1,6 @@
+	<div class="hidden-phone">	
+		<a href="#howitworks-section"><img class="img-border sidebar_image img-pull-up img-pull-up-top" src="./img/sidebar/lifetimesidenav.png"></a>
+		<a href="#data-section"><img class="img-border sidebar_image img-pull-up" src="./img/sidebar/lifetimesidenav.png"></a>
+		<a href="#surfboard-section"><img class="img-border sidebar_image img-pull-up" src="./img/sidebar/lifetimesidenav.png"></a>
+		<a href="#leaderboard-section"><img class="img-border sidebar_image" src="./img/sidebar/lifetimesidenav.png"></a>
+	</div>
