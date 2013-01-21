b42ad009559f641469bc6a9a3323cd4c95047bda
marco.morawec <marco.morawec@gmail.com>
2013-01-20 21:04:10 -0500

    new market size map

---
+++ market.php
@@ -0,0 +1,63 @@
+
+
+<?php include ('./includes/headers.inc.php') ?>
+
+<body>
+
+ <div class="board-background">
+
+
+<?php include ('./includes/navbar.inc.php') // get the navbar in ?>
+
+
+    <div class="well-clear frost-layer container">
+    	
+    	<div class="span12">
+      
+	    	<div class="headline market">
+	    		<h1 class="custom-h1">Market Size</h1>  
+	      	</div>
+	      	
+	 
+	      	
+	      	<!-- Start of Map -->
+		      	<div class="circle" style="top:55%; left:17%;">
+		      		<h5 class="custom-h5 market-text">12.5</h5>
+		      	</div>
+		      	<div class="circle" style="top:45%; left:38%;">
+		      		<h5 class="custom-h5">4.75</h5>
+		      	</div>
+		      	<div class="circle" style="top:65%; left:41%;">
+		      		<h5 class="custom-h5">4.75</h5>
+		      	</div>
+		      	<div class="circle" style="top:52%; left:55%;">
+		      		<h5 class="custom-h5">6.5</h5>
+		      	</div>
+		      	<div class="circle" style="top:78%; left:62%;">
+		      		<h5 class="custom-h5">6.5</h5>
+		      	</div>
+	      	
+	      	<div class="span9">
+	      		<img class="map" src="./img/market/worldmap.png"></img>
+	      	</div>
+	      	
+	      	<div class="span2 market">
+	      		<h3 class="custom-h3 sidebar-box">26 Million Surfers Worldwide in 2000</h3>
+	      		<h3 class="custom-h3 sidebar-box">30 Million Surfers Worldwide in 2012</h3>
+	      		<h3 class="custom-h3 sidebar-box">40 Million Surfers Worldwide in 2020</h3>
+	      		
+	      	</div>
+	      	
+	      	</div><!-- End of Map -->
+	    </div> <!-- end of span12 -->
+
+
+
+
+
+    </div>
+  </div>  <!-- End of board section -->
+<?php include ('./includes/footer.inc.php') // get the footer in ?>
+    
+  </body>
+</html>
