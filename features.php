7a49ade1364c0deb34bddde6eed8c5e9b963e4e8
marco.morawec <marco.morawec@gmail.com>
2013-01-20 22:02:47 -0500

    very rough features page outline

---
+++ features.php
@@ -0,0 +1,32 @@
+
+
+<?php include ('./includes/headers.inc.php') ?>
+
+<body>
+
+ <div class="board-background">
+
+
+<?php include ('./includes/navbar.inc.php') // get the navbar in ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="span6">
+				<h1>Here goes the data stuff on the iPad</h1>
+			</div>
+			<div class="span6">
+				<h1>Here goes the text explaining the data stuff on the ipdad</h1>
+			</div>
+	</div>
+
+
+    <div class="well-clear frost-layer container">
+      
+      <h1>Here goes the features stuff</h1>
+      
+      
+  </div>  <!-- End of board section -->
+<?php include ('./includes/footer.inc.php') // get the footer in ?>
+    
+  </body>
+</html>
