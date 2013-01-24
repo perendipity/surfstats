be3aafa222744de6d40ca8c19364834c04edf73a
Cody Canning <cocanning11@gmail.com>
2013-01-24 12:32:42 -0800

    added team-pitch and scoreboard.css

---
+++ team-pitch.php
@@ -0,0 +1,74 @@
+<?php include './includes/template.team.inc.php' ?> 
+
+<style>
+	body {
+			background-color: black;
+			color: #FFFFFF;
+		}
+	
+</style>
+
+<!-- Start of Team page -->
+	<?php startblock('team_title') ?> 
+		Our Team
+	<?php endblock() ?> 
+
+	<?php startblock('team_h2') ?> 
+		
+	<?php endblock() ?>
+	
+	 
+<!-- Marco -->
+	<?php startblock('team_marco_img') ?> 
+	<img class="team-image" src="./img/team/marco_bw.jpg">
+	<?php endblock() ?> 
+	
+	<?php startblock('team_marco_h2') ?> 
+		Marco Morawec <a href="http://www.twitter.com/marcomorawec"  target="_blank"><img src="./img/team/smi-t.png"></a> <a href="http://www.linkedin.com/in/marcomorawec" target="_blank"><img src="./img/team/smi-in.png"></a>
+	<?php endblock() ?>
+	
+	<?php startblock('team_marco_copy') ?> 
+	<h3 class="custom-h3">Surfer and experienced product manager</h3>
+	<?php endblock() ?> 
+	
+<!-- Peter -->
+	<?php startblock('team_peter_img') ?> 
+		<img class="team-image" src="./img/team/peter_bw.jpg" >
+	<?php endblock() ?> 
+
+	<?php startblock('team_peter_h2') ?> 
+		Peter Nachbaur <a href="http://www.twitter.com/peternachbaur" target="_blank"><img src="./img/team/smi-t.png"></a> <a href="http://www.linkedin.com/in/peternachbaur" target="_blank"><img src="./img/team/smi-in.png"></a>
+	<?php endblock() ?>
+	
+	<?php startblock('team_peter_copy') ?> 
+	<h3 class="custom-h3">Big data at Warner Bros.</h3>
+	<?php endblock() ?> 
+	
+<!-- Cody -->
+	<?php startblock('team_cody_img') ?> 
+		<img class="team-image" src="./img/team/cody_bw.jpg" >
+	<?php endblock() ?> 
+
+	<?php startblock('team_cody_h2') ?> 
+		Cody Canning <a href="http://www.linkedin.com/in/codycanning"" target="_blank"><img src="./img/team/smi-in.png"></a>
+	<?php endblock() ?>
+	
+	<?php startblock('team_cody_copy') ?> 
+        <h3 class="custom-h3">Machine learning, pursuing Ph.D. at Tufts</h3>
+	<?php endblock() ?> 
+	
+<!-- Alec -->
+	<?php startblock('team_alec_img') ?> 
+		<img class="team-image" src="./img/team/alec.jpg" >
+	<?php endblock() ?> 
+
+	<?php startblock('team_alec_h2') ?> 
+		Alec Babala <a href="http://www.twitter.com/alecbabala" target="_blank"><img src="./img/team/smi-t.png"></a> <a href="http://www.linkedin.com/in/alecbabala" target="_blank"><img src="./img/team/smi-in.png"></a>
+	<?php endblock() ?>
+	
+	<?php startblock('team_alec_copy') ?> 
+	<h3 class="custom-h3 pitch-blurb">RISD design wizard</h3>
+	<?php endblock() ?> 
+        
+
+<!-- End of Team page -->
