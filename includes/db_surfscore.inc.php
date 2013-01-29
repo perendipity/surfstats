abfbeff18fa9ff591218deeb2a8889c44527df75
marco.morawec <marco.morawec@gmail.com>
2013-01-28 19:59:05 -0500

    added .htaccess - home.php is now index

--- includes/db_surfscore.inc.php
+++ includes/db_surfscore.inc.php
@@ -20,8 +20,25 @@ if (!mysql_query($sql,$con))
   {
   die('Error: ' . mysql_error());
   }
+  
+		// Sending the email
+						
+			$body = "Hi there,
+			
+Huge thanks for being as excited about surfscore as we are!
+			
+We are working extra hard to help surfers shred better and welcome you to the surfscore team.
+If you would like to be part of our elite surf team collecting data and receiving kick-ass feedback of all your sessions in return, please reply to this email and tell us why you want to be part of surfscores elite surfer team.
+			
+Aloha,
+Marco, Peter, Cody and Alec - Your surfscore team";
+			
+			mail($_POST['email'], 'Aloha! - Join Surfscores Elite Surfing Team', $body, 'From: hello@surfscore.me');  
+  
+  
+  
   //All Good? --> Refresh the page - > But we should display a thank you message with jQuery now.
-			header ('Location: ../registration_new.php'); 
+			header ('Location: ../home.php'); 
 			exit();
 
 mysql_close($con);
